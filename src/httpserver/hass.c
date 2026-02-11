da65225cedbd915fd2bac06c42b40a9fc124f991
Marcin685 <116270766+Marcin685@users.noreply.github.com>
2026-02-11 22:14:20 +0100

    Refactor uniq_id formatting for light types

--- src/httpserver/hass.c
+++ src/httpserver/hass.c
@@ -43,12 +43,10 @@ void hass_populate_unique_id(ENTITY_TYPE type, int index, char* uniq_id, int ase
 	switch (type) {
 	case LIGHT_ON_OFF:
 	case LIGHT_PWM:
-		sprintf(uniq_id, "%s_%d", longDeviceName, "light", index);
-		break;
 	case LIGHT_PWMCW:
 	case LIGHT_RGB:
 	case LIGHT_RGBCW:
-		sprintf(uniq_id, "%s", longDeviceName, "light");
+		sprintf(uniq_id, "%s", longDeviceName);
 		break;
 		
 	case HASS_FAN:
