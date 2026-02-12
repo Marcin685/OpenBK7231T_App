d9d1bfe93f63db6cfd94105634046cc1a16afeb0
Marcin685 <116270766+Marcin685@users.noreply.github.com>
2026-02-12 09:54:10 +0100

    tylko friendly name zmiana

--- src/httpserver/hass.c
+++ src/httpserver/hass.c
@@ -530,8 +530,8 @@ HassDeviceInfo* hass_init_device_info(ENTITY_TYPE type, int index, const char* p
 		case LIGHT_RGBCW:
 			//There can only be one RGB so we can skip including index in the name. Do the same
 			//for 2 PWM case.
-			sprintf(g_hassBuffer, "Light");
-			//sprintf(g_hassBuffer, "%s", CFG_GetDeviceName());
+			//sprintf(g_hassBuffer, "Light");
+			sprintf(g_hassBuffer, "%s", CFG_GetDeviceName());
 			break;
 		case ENERGY_METER_SENSOR:
 			isSensor = true;
